5fa9d1e43eaf12d77f5a5b1eeeb3fd89314821b4
glitch86x <zahimzakaria94@gmail.com>
2026-09-04 17:47:44 -0400

    loop

--- f.c
+++ f.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h> 
+#include <stdlib.h>
+#include <time.h>
+
 int main(){
+    	
 
- char user[50] = "";
- bool on = true;
- char res = '\0';
+   int num[] = {10, 20, 30, 40, 50};
+
+   printf("%d", num[3]);
 
 
-  printf("user name for the machine ;");
-  fgets(user , sizeof(user) , stdin);
-  user[strcspn(user, "\n")] = '\0';
 
-  while(strlen(user) == 0 ){
-       printf("no name tedected , try again ; ");
-       fgets(user , sizeof(user) , stdin);
-       user[strcspn(user, "\n")] = '\0';
-  }
 
- while(on){
-	printf("you play bed war");
-	printf("would you to continue y = yes , n = no ");
-	scanf("%c", &res);
-  if(res != 'y')
-	  on = false;
+ return 0;
 
- }
-  printf("hello %s \n ", user);
 
-  printf("you exit and save the game");
-
-	return 0;
 }
